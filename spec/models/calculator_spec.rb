621e5a8ec5953a12eaa1c886d73b8e3b1a7db0ba
Akash Kumrawat <akash.kumrawat.sdbg@gmail.com>
2024-05-28 12:34:02 +0530

    Allow the add method to handle any amount of numbers.

--- spec/models/calculator_spec.rb
+++ spec/models/calculator_spec.rb
@@ -13,5 +13,11 @@ RSpec.describe Calculator, type: :model do
     it "returns the sum of numbers for a comma-separated string" do
       expect(Calculator.add("1,5")).to eq(6)
     end
+
+    it "returns the sum of numbers for any amount of numbers" do
+      expect(Calculator.add("1,2,3,4,5")).to eq(15)
+      expect(Calculator.add("10,20,30,40,50")).to eq(150)
+      expect(Calculator.add("2,4,6,8,10,12")).to eq(42)
+    end
   end
 end
